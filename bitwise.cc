2cdd436175f3f5e0b2b72c6cc446af8a5516c821
ShantakshKamat <85739236+ShantakshKamat@users.noreply.github.com>
2022-09-07 00:26:42 +0530

    Create bitwise.cc

---
+++ bitwise.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 2;
+    int b = 6;
+
+    cout << " a&b is: " << (a & b) << endl;
+    cout << " a|b is: " << (a | b) << endl;
+    cout << " ~b is: " << (~b) << endl;
+    cout << " a^b is: " << (a ^ b) << endl;
+
+    return 0;
+}
